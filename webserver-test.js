1fa674a91809496fc218b12b1ec05d131edfd0fb
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-06 17:10:21 +1100

    modular contenttype

--- webserver-test.js
+++ webserver-test.js
@@ -4,23 +4,47 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var url = require('url');
+var path = require('path');
 
 http.listen(8080); //listen to port 8080
 
 function handler(req, res) { //create server (request, response)
+    // Get the location of the request
     var loc = url.parse(req.url, true);
     if (loc.pathname == "/") {
+        // index page
         var filename = "." + "/example-web/" + "index.html";
     } else {
         var filename = "." + "/example-web/" + loc.pathname;
     }
+
+    // Content type
+    var type;
+    switch (path.extname(filename)) {
+        case ".html":
+            type = "text/html";
+            break;
+        case ".js":
+            type = "text/javascript";
+            break;
+        case ".jpg":
+            type = "image/jpg";
+            break;
+        case ".png":
+            type = "image/png";
+            break;
+        case ".json":
+            type = "application/json";
+            break;
+    }
+
     // fs.readFile(__dirname + '/example-web/index.html', function (err, data) { //read file index.html in public folder
     fs.readFile(filename, function (err, data) { //read file index.html in public folder
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
             return res.end("404 Not Found");
         }
-        res.writeHead(200, { 'Content-Type': 'text/html' }); //write HTML
+        res.writeHead(200, { 'Content-Type': type }); //write HTML
         res.write(data); //write data from index.html
         return res.end();
     });
