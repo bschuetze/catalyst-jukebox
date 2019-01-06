b5d8ea3111ca94ee53bd2a7113357c08db324233
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-06 16:47:41 +1100

    udpate webserver

--- webserver-test.js
+++ webserver-test.js
@@ -7,7 +7,7 @@ var fs = require('fs'); //require filesystem module
 http.listen(8080); //listen to port 8080
 
 function handler(req, res) { //create server
-    fs.readFile(__dirname + '/example-web/index.html', function (err, data) { //read file index.html in public folder
+    fs.readFile(__dirname + '/example-web/', function (err, data) { //read file index.html in public folder
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' }); //display 404 on error
             return res.end("404 Not Found");
