0144ccb5e15ae250cee57c778d7f8d4c17b69031
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-14 22:34:16 +1100

    more auth work

--- example-web/sketch.js
+++ example-web/sketch.js
@@ -1,11 +1,21 @@
 var token;
 var authorized;
 var authorizeURL;
+var clientID; // Located at: https://developer.spotify.com/dashboard/applications/
+var redirectURI;
+var scope;
+var currentWindow;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     // any additional setup code goes here
     authorized = false;
+    authorizeURL = "https://accounts.spotify.com/authorize";
+    clientID = "";
+    redirectURI = "redirect_uri=http://127.0.0.1:5500/example-web/";
+    scope = "scope=user-modify-playback-state playlist-read-private user-read-recently-played user-read-currently-playing";
+    currentWindow = window.location;
+    console.log(currentWindow);
 }
 
 function draw() {
@@ -13,19 +23,23 @@ function draw() {
     rect(100, 100, 100, 200);
 }
 
+function updateClientID(cid) {
+    clientID = "client_id=" + cid;
+}
+
 function mouseClicked() {
     if (!authorized) {
-        fetch("https://accounts.spotify.com/authorize", {
-            headers: { "Content-Type": "application/json; charset=utf-8",
-                       "Authorization": token},
+        console.log(authorizeURL + "?" + clientID + "&response_type=code&" + redirectURI + "&" + scope)
+        fetch(authorizeURL + "?" + clientID + "&response_type=code&" + redirectURI + "&" + scope, {
+            // headers: { "Content-Type": "application/json; charset=utf-8"},
             method: "GET"
             // body: JSON.stringify({
             //     username: 'Elon Musk',
             //     email: 'elonmusk@gmail.com',
             // })
         })
-        .then(response => response.json())
-        .then(data => console.log(data));
+        // .then(response => response.json())
+        // .then(data => console.log(data));
     } else {
         fetch("https://api.spotify.com/v1/me/player/recently-played", {
             headers: { "Content-Type": "application/json; charset=utf-8",
