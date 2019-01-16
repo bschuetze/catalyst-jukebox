c73625b2c27e7ea06090108c6a094bd9ec6a9fda
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-16 22:24:10 +1100

    test font size

--- example-web/login.js
+++ example-web/login.js
@@ -2,6 +2,7 @@ var input;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    textSize(24);
     input = createInput();
     input.size(width * 0.4, height * 0.1);
     input.position(width * 0.3, height * 0.3);
