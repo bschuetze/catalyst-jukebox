de60e1383ab3163fa3be2ddb2fe9271a03fecb23
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-25 20:46:39 +1100

    finalizing auth with server

--- webserver-test.js
+++ webserver-test.js
@@ -100,6 +100,47 @@ function webRequest(dest, method, header, body, respFunc) {
     // }).then(response => webResponse(response, authRedirect));
 }
 
+function emptyObject(o) {
+    if (o == null) {
+        // If null value is present then no object
+        return true;
+    }
+    if (o.constructor == Object && Object.entries(o).length == 0) {
+        // If is of type object and has no entries
+        return true;
+    }
+    return false;
+}
+
+function webResponse(response, respFunc) {
+    // Response method originally found from here: https://stackoverflow.com/questions/37121301/how-to-check-if-the-response-of-a-fetch-is-a-json-object-in-javascript
+    let contentType = response.headers.get("content-type");
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+        console.log("JSON Response");
+        console.log(response);
+        return response.json().then(data => {
+            // process your JSON data further
+            if (respFunc !== undefined) {
+                respFunc(data);
+            } else {
+                console.log(data);
+            }
+        });
+    } else {
+        return response.text().then(text => {
+            // this is text, do something with it
+            console.log("Other Response");
+            // console.log("Content: " + text + "\nResponse header: " + response.headers.get("content-type"));
+            // console.log(response.headers);
+            if (respFunc !== undefined) {
+                respFunc(text);
+            } else {
+                console.log(text);
+            }
+        });
+    }
+}
+
 // function spotifyAuth() {
 
 //     if (clientID == "") {
