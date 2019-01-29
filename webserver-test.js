c453d961b816347f1cc1f8be4aafbbcde97f7153
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-29 18:26:20 +1100

    request page make

--- webserver-test.js
+++ webserver-test.js
@@ -285,7 +285,7 @@ function handler(req, res) { //create server (request, response)
     // Get the location of the request
     var loc = url.parse(req.url, true);
 
-    console.log("Request from: " + loc);
+    console.log("Request from: " + loc.href);
 
     _favicon(req, res, function cont(favError) {
         if (favError) return done(favError);
