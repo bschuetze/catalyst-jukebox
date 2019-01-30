6b08967191f70431c4e4ecfb5aa0dd05c7e1689f
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-30 16:56:12 +1100

    python work

--- device-monitoring/monitoring.py
+++ device-monitoring/monitoring.py
@@ -13,14 +13,16 @@ print (context)
 print (monitor)
 
 for device in context.list_devices(subsystem='usb'):
-    print (device)
-    print ("Vendor: " + str(device.get('ID_VENDOR_ID')))
-    print ("Serial: " + str(device.get('ID_SERIAL')))
-    print ("Model: " + str(device.get('ID_MODEL')))
-    print ("Type: " + str(device.get('ID_TYPE')))
-    print ("Path: " + str(device.get('ID_PATH')))
+    print(device)
+    print("Vendor: " + str(device.get('ID_VENDOR_ID')))
+    print("Serial: " + str(device.get('ID_SERIAL')))
+    print("Model: " + str(device.get('ID_MODEL')))
+    print("Type: " + str(device.get('ID_TYPE')))
+    print("Path: " + str(device.get('ID_PATH')))
 
     if (device.get("ID_PATH") is not None):
         devicePathFull = device.get("ID_PATH")
         pathSplit = devicePathFull.split(":")
-        print (pathSplit[len(pathSplit) - 1])
+        print(pathSplit[len(pathSplit) - 1])
+        if (float(pathSplit[len(pathSplit) - 1]) >= 1.2):
+            print("Monitoring " + device.get('ID_MODEL') + " at usb port " + pathSplit[len(pathSplit) - 1]))
