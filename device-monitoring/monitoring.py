fc0ff1a9286c54482fc57af4f59955f087dbe45a
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-01-30 17:28:01 +1100

    python work

--- device-monitoring/monitoring.py
+++ device-monitoring/monitoring.py
@@ -5,6 +5,7 @@ import pyudev
 #import
 
 # Adding a blacklist for usb device models (device.get('ID_MODEL'))))
+# Add your USB hub here if using one or any other devices you want to ignore
 USB_BLACKLIST = ["USB_2.0_Hub__Safe_"]
 
 context = pyudev.Context()
@@ -15,25 +16,43 @@ monitor.filter_by('usb')
 print (context)
 print (monitor)
 
-for device in context.list_devices(subsystem='usb'):
-    print(device)
-    print("Vendor: " + str(device.get('ID_VENDOR_ID')))
-    print("Serial: " + str(device.get('ID_SERIAL')))
-    print("Model: " + str(device.get('ID_MODEL')))
-    print("Type: " + str(device.get('ID_TYPE')))
-    print("Path: " + str(device.get('ID_PATH')))
+def usb_event(action, device):
+    devicePathFull = device.get("ID_PATH")
+    pathSplit = devicePathFull.split(":")
+    print(pathSplit[len(pathSplit) - 1])
+    try:
+        usbPort = pathSplit[len(pathSplit) - 1]
+        usbPort = usbPort.replace(".", "")
 
-    if (device.get("ID_PATH") is not None):
-        devicePathFull = device.get("ID_PATH")
-        pathSplit = devicePathFull.split(":")
-        print(pathSplit[len(pathSplit) - 1])
-        try:
-            usbPort = pathSplit[len(pathSplit) - 1]
-            usbPort = usbPort.replace(".", "")
+        if ((int(usbPort) >= 12) and (device.get('ID_MODEL') not in USB_BLACKLIST)):
+            print("Monitoring " + device.get('ID_MODEL') + " at usb port " + pathSplit[len(pathSplit) - 1])
+    except:
+        print(pathSplit[len(pathSplit) - 1] + " is not a number of the form X.Y or X.Y.Z")
 
-            # if (float(pathSplit[len(pathSplit) - 1]) >= 1.2):
-            if ((int(usbPort) >= 12) and (device.get('ID_MODEL') not in USB_BLACKLIST)):
-                print("Monitoring " + device.get('ID_MODEL') + " at usb port " + pathSplit[len(pathSplit) - 1])
-        except:
-            print(pathSplit[len(pathSplit) - 1] + " is not a number of the form X.Y or X.Y.Z")
-    print("-------------------")
+
+usbObs = pyudev.MonitorObserver(monitor, usb_event)
+usbObs.start()
+
+
+# for device in context.list_devices(subsystem='usb'):
+#     print(device)
+#     print("Vendor: " + str(device.get('ID_VENDOR_ID')))
+#     print("Serial: " + str(device.get('ID_SERIAL')))
+#     print("Model: " + str(device.get('ID_MODEL')))
+#     print("Type: " + str(device.get('ID_TYPE')))
+#     print("Path: " + str(device.get('ID_PATH')))
+
+#     if (device.get("ID_PATH") is not None):
+#         devicePathFull = device.get("ID_PATH")
+#         pathSplit = devicePathFull.split(":")
+#         print(pathSplit[len(pathSplit) - 1])
+#         try:
+#             usbPort = pathSplit[len(pathSplit) - 1]
+#             usbPort = usbPort.replace(".", "")
+
+#             # if (float(pathSplit[len(pathSplit) - 1]) >= 1.2):
+#             if ((int(usbPort) >= 12) and (device.get('ID_MODEL') not in USB_BLACKLIST)):
+#                 print("Monitoring " + device.get('ID_MODEL') + " at usb port " + pathSplit[len(pathSplit) - 1])
+#         except:
+#             print(pathSplit[len(pathSplit) - 1] + " is not a number of the form X.Y or X.Y.Z")
+#     print("-------------------")
