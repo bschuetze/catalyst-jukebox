d4f64e7925774afd9760facc94e691ed2c9f7bed
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-08 18:11:05 +1100

    ugh

--- device-monitoring/test.py
+++ device-monitoring/test.py
@@ -19,7 +19,7 @@ client = mqtt.Client(client_id=CLIENT_ID)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect("192.168.0.5", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
