e88b8878acdb39a7038d975537e2f5247b9a2e34
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-08 13:06:05 +1100

    switching to diff lib

--- esp32-arduino/catalyst-jukebox-pager/src/main.cpp
+++ esp32-arduino/catalyst-jukebox-pager/src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
+#include "SimpleBLE.h"
 
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+SimpleBLE ble;
 long ID;
+String NAME;
 
 void setup() {
   // Baudrate, defines bits per second possible to transfer bia serial
@@ -9,7 +16,12 @@ void setup() {
 
   ID = random(99999999);
   Serial.println(ID);
-  
+
+  NAME = "catalyst-pager_";
+  NAME = NAME + ID;
+  Serial.print(NAME);
+  ble.begin(NAME);
+
   pinMode(5, OUTPUT);
   digitalWrite(5, 0);
 }
@@ -17,8 +29,6 @@ void setup() {
 void loop() {
   delay(1000);
   int pinState = digitalRead(5);
-  Serial.println(pinState);
   pinState = abs(pinState - 1);
   digitalWrite(5, pinState);
-  Serial.println(pinState);
 }
