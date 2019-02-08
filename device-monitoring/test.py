9a7a4f143c1e91c06c3a60c03aa6e9ba80ad5901
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-08 18:08:11 +1100

    ?

--- device-monitoring/test.py
+++ device-monitoring/test.py
@@ -15,7 +15,8 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
 
-client = mqtt.Client()
+client = mqtt.Client(client_id=CLIENT_ID, clean_session=True,
+                     userdata=None, protocol=mqtt.MQTTv31, transport="websockets")
 client.on_connect = on_connect
 client.on_message = on_message
 
