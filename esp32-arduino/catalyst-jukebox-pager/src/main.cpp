07b8d3e51817e5ed6e6530385a556ba4348add8a
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-08 12:02:13 +1100

    work on arduino for esp32

--- esp32-arduino/catalyst-jukebox-pager/src/main.cpp
+++ esp32-arduino/catalyst-jukebox-pager/src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
 
 void setup() {
+  // Baudrate, defines bits per second possible to transfer bia serial
   Serial.begin(9600);
+  pinMode(5, OUTPUT);
 }
 
 void loop() {
   delay(1000);
-  Serial.println("Hello World!");
+  int pinState = digitalRead(5);
+  Serial.println(pinState);
+  pinState = abs(pinState - 1);
+  digitalWrite(5, pinState);
+  Serial.println(pinState);
 }
