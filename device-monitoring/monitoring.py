681e556a1a3599ac54b4ab86f0e568a2605b69c4
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-08 17:41:22 +1100

    pls

--- device-monitoring/monitoring.py
+++ device-monitoring/monitoring.py
@@ -237,7 +237,7 @@ client = mqtt.Client()
 client.on_connect = onConnect
 client.on_disconnect = onDisconnect
 client.on_message = onMessage
-client.connect("localhost", 1883, 60)
+client.connect("192.168.0.5", 1883, 60)
 # client.loop_start()
 client.loop_forever()
 
