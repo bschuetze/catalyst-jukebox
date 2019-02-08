9a8ede13608397eefba3c4f2329b9bfacfc48c8c
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-08 17:56:45 +1100

    fuuuuuuuk

--- device-monitoring/test.py
+++ device-monitoring/test.py
@@ -1,16 +1,14 @@
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
-
+GLOBAL_TOPIC = "catalyst-jukebox_global"
+PAGER_TOPICS = []
+CLIENT_ID = "catalyst-jukebox_MAIN"
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
+    print("MQTT connected with code: " + rc)
+    client.subscribe(GLOBAL_TOPIC)
     client.subscribe("$SYS/#")
 
-# The callback for when a PUBLISH message is received from the server.
 
 
 def on_message(client, userdata, msg):
@@ -21,7 +19,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("iot.eclipse.org", 1883, 60)
+client.connect("192.168.0.5", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
