9606da686d96fa2d610a21c730ca0426697014d9
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-13 17:43:42 +1100

    more work on pager

--- esp32-micropython/workSpace/main.py
+++ esp32-micropython/workSpace/main.py
@@ -2,6 +2,7 @@
 # /c/Python3/Scripts/esptool.py.exe --chip esp32 -p COM3 erase_flash
 # /c/Python3/Scripts/esptool.py.exe --chip esp32 -p COM3 write_flash -z 0x1000 /x/University/COMPCHST/catalyst-jukebox/esp32-micropython/esp32-20190207-v1.10-54-g43a894fb4.bin
 # https://hackernoon.com/get-on-the-good-foot-with-micropython-on-the-esp32-decdd32c4720
+# http://www.learningaboutelectronics.com/Articles/Vibration-motor-circuit.php
 
 import machine
 from machine import Pin
@@ -34,16 +35,51 @@ def vibrate(on):
         # stop pulse
         print("Turning off vibration")
 
-def pulse(device, cont, interval):
-    # if (cont):
-    #     device.value(not device.value())
-    #     timer.callback(pulse(device, cont, interval))
-    # else:
-    print("Pulsing")
+def pulse(start, timer, mode, interval, cb):
+    if (start):
+        print("Pulsing")
+        timer.init(period=interval, mode=mode, callback=cb)
+    else:
+        print("Stopping pulse")
+        timer.deinit()
+
 
 def onboardLEDPulse(timer):
     ONBOARD_LED.value(not ONBOARD_LED.value())
     
+def vibrationPulse(timer):
+    VIBR_CONTROL.value(not VIBR_CONTROL.value())
+    
+def buzzerPulse(timer):
+    BUZZ_CONTROL.value(not BUZZ_CONTROL.value())
+
+def alertLEDPulse(timer):
+    ledCtrl.alertLEDPulse(timer)
+
+class LED_Control:
+    def __init__(self):
+        self.ONBOARD_LED_DURATION = 5
+        self.ONBOARD_LED_COUNT = 0
+
+    def alertLEDPulse(self, timer):
+        self.ONBOARD_LED_COUNT = self.ONBOARD_LED_COUNT + 1
+        if (self.ONBOARD_LED_COUNT == self.ONBOARD_LED_DURATION):
+            self.ONBOARD_LED_COUNT = 0
+            ONBOARD_LED.value(not ONBOARD_LED.value())
+
+        if (pager.IDENTIFYING or pager.BUZZING):
+            ALERT_LED1.value(not ALERT_LED1.value())
+            ALERT_LED2.value(not ALERT_LED2.value())
+
+
+def usbHandler(pin):
+    pager.detectConnection(USB_PIN.value())
+    if (USB_PIN.value() == 0):
+        GREEN_LED.value(0)
+        WHITE_LED.value(1)
+    elif (USB_PIN.value() == 1):
+        GREEN_LED.value(1)
+        WHITE_LED.value(0)
 
 
 connectToWiFi(utils.WIFI_PWD)
@@ -56,6 +92,7 @@ class Pager:
         self.IDENTIFYING = False
         self.OWNER = ""
         self.CONNECTED = False
+        self.MQTT_READY = False
 
     def resetPager(self):
         print("Resetting pager")
@@ -75,13 +112,19 @@ class Pager:
         self.IN_USE = False
         self.OWNER = ""
 
-    def detectConnection(self):
+    def detectConnection(self, val):
         # Read pins, assign CONNECTED Accordingly
-        print("Reading Pins")
-        usbPinValue = False
+        print("USB Pin value: " + str(val))
+        if (val == 0):
+            usbPinValue = False
+        else:
+            usbPinValue = True
+
         if (usbPinValue != self.CONNECTED):
             self.CONNECTED = usbPinValue
-            updateConnection()
+            if (self.MQTT_READY):
+                updateConnection()
+
             if (self.CONNECTED):
                 print("Connected")
             else:
@@ -96,37 +139,63 @@ class Pager:
             print("Buzzing")
             self.BUZZING = True
             # Make buzzer calls
+            VIBR_CONTROL.value(1)
+            BUZZ_CONTROL.value(1)
+            ALERT_LED1.value(1)
+            ALERT_LED2.value(1)
+            pulse(True, vibrationTimer, machine.Timer.PERIODIC, 500, vibrationPulse)
+            pulse(True, buzzerTimer, machine.Timer.PERIODIC, 2, buzzerPulse)
         else:
             print("Ending buzz")
             self.BUZZING = False
             # Clear buzzer stuff
+            VIBR_CONTROL.value(0)
+            BUZZ_CONTROL.value(0)
+            ALERT_LED1.value(0)
+            ALERT_LED2.value(0)
+            pulse(False, vibrationTimer, machine.Timer.PERIODIC, 500, vibrationPulse)
+            pulse(False, buzzerTimer, machine.Timer.PERIODIC, 2, buzzerPulse)
 
     def identify(self, start):
         if (start):
             print("Identifying")
             self.IDENTIFYING = True
-            # Light LEDS
             # Start detect Connection
         else:
             print("Ending identify")
             self.IDENTIFYING = False
-            # Kill LEDS
 
 # Devices
 pager = Pager()
+ledCtrl = LED_Control()
 
 ONBOARD_LED = Pin(5, Pin.OUT)
 ONBOARD_LED.value(0)
-onboardLEDTimer = machine.Timer(0)
+ALERT_LED1 = Pin(18, Pin.OUT)
+ALERT_LED2 = Pin(2, Pin.OUT)
+ALERT_LED1.value(0)
+ALERT_LED2.value(0)
+
+# onboardLEDTimer = machine.Timer(0)
+LEDTimer = machine.Timer(0)
+vibrationTimer = machine.Timer(1)
+buzzerTimer = machine.Timer(2)
 
 USB_PIN = Pin(13, Pin.IN)
+
 GREEN_LED = Pin(16, Pin.OUT)
 GREEN_LED.value(0)
 WHITE_LED = Pin(17, Pin.OUT)
 WHITE_LED.value(0)
 
+VIBR_CONTROL = Pin(4, Pin.OUT)
+VIBR_CONTROL.value(0)
+
+BUZZ_CONTROL = Pin(21, Pin.OUT)
+VIBR_CONTROL.value(0)
+
 # WiFi
-loops = 10
+loops = 10 # CHANGE BACK TO 0
 while (not station.isconnected() and loops < 10):
     # Give some time to connect to Network and clear out log
     time.sleep(5)
@@ -140,8 +209,17 @@ print()
 print()
 print("------- STARTING -------")
 
+
 # Timers
-onboardLEDTimer.init(period=1000, mode=machine.Timer.PERIODIC, callback=onboardLEDPulse)
+# pulse(True, onboardLEDTimer, machine.Timer.PERIODIC, 1000, onboardLEDPulse)
+# pulse(True, LEDTimer, machine.Timer.PERIODIC, 1000, onboardLEDPulse)
+pulse(True, LEDTimer, machine.Timer.PERIODIC, 200, alertLEDPulse)
+# pulse(True, vibrationTimer, machine.Timer.PERIODIC, 2000, vibrationPulse)
+# pulse(True, buzzerTimer, machine.Timer.PERIODIC, 2, buzzerPulse)
+
+# onboardLEDTimer.init(period=1000, mode=machine.Timer.PERIODIC, callback=onboardLEDPulse)
+# vibrationTimer.init(period=2000, mode=machine.Timer.PERIODIC, callback=vibrationPulse)
+# buzzerTimer.init(period=2, mode=machine.Timer.PERIODIC, callback=buzzerPulse)
 
 # try:
 #   SEED = SEED + time.ticks_us()
@@ -182,10 +260,16 @@ def messageReceived(topic, msg):
     if (splitTopic[-1] == "control"):
       if (decMsg == INIT_MSG):
         pager.resetPager()
-        pager.detectConnection()
-        updateConnection()
+        pager.detectConnection(USB_PIN)
+        # updateConnection()
     if (splitTopic[-1] == "checkout"):
         pager.assign(decMsg)
+
+
+# Interrupt handlers
+# USB_PIN.irq(trigger=(Pin.IRQ_FALLING | Pin.IRQ_RISING), handler=usbHandler, priority=1, wake=(machine.IDLE | machine.SLEEP), hard=False)
+USB_PIN.irq(trigger=(Pin.IRQ_FALLING | Pin.IRQ_RISING), handler=usbHandler)
+
 
 while True:
     # ONBOARD_LED.value(not ONBOARD_LED.value())
@@ -204,19 +288,18 @@ while True:
         client.set_callback(messageReceived)
         client.subscribe(bytes(str(TOPIC_BASE + "/" + str(ID) + "/control"), "utf-8"))
         client.subscribe(bytes(str(TOPIC_BASE + "/" + str(ID) + "/checkout"), "utf-8"))
+        pager.MQTT_READY = True
 
     if (publishedName and station.isconnected()):
         client.check_msg()
-    
-    if (USB_PIN.value() == 0):
-      GREEN_LED.value(0)
-      WHITE_LED.value(1)
-    elif (USB_PIN.value() == 1):
-      GREEN_LED.value(1)
-      WHITE_LED.value(0)
-      
+
+    # pager.buzz(not pager.BUZZING)
     
     time.sleep(5)
+
+
+
+
 
 
 
