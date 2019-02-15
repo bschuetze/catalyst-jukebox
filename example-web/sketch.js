0d6064bf9c912a36b9a3653576d00584a624ae44
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-15 16:18:08 +1100

    sketch ip test

--- example-web/sketch.js
+++ example-web/sketch.js
@@ -6,10 +6,15 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     currentWindow = window.location;
     console.log(currentWindow);
-    
+    let destinationURL = window.location.origin
+    webRequest(destinationURL, "POST", {}, {}, getIPCallback);
 }
 
 function draw() {
     fill(255, 0, 0);
     rect(100, 100, 100, 200);
+}
+
+function getIPCallback(data) {
+    console.log(data);
 }
