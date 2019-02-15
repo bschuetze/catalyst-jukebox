97b5091ec8f23b7a04e5e135dc535181c1762f4c
Brent Schuetze <brent.schuetze@anu.edu.au>
2019-02-15 22:49:33 +1100

    API test

--- example-web/sketch.js
+++ example-web/sketch.js
@@ -5,8 +5,12 @@ var localIP = "";
 var publicIP = "";
 var qrPublic = null;
 var qrLocal = null;
-var qrLocalDiv;
-var qrPublicDiv;
+// var qrLocalDiv;
+// var qrPublicDiv;
+
+const qrAPI = "http://api.qrserver.com/v1/create-qr-code/"
+// ?data=text&size=0x0"
+const qrSize = "256x256";
 
 
 function setup() {
@@ -15,29 +19,35 @@ function setup() {
     let destinationURL = window.location.origin + ipReq;
     webRequest(destinationURL, "POST", {}, {}, getIPCallback);
 
-    qrLocalDiv = createDiv();
-    qrPublicDiv = createDiv();
+    // qrLocalDiv = createDiv();
+    // qrPublicDiv = createDiv();
 }
 
 function draw() {
     if (localIP != "" && qrLocal == null) {
-        qrLocal = new QRCode(qrLocalDiv, {
-            text: localIP,
-            width: 128,
-            height: 128,
-            colorDark: "#000000",
-            colorLight: "#ffffff"
-        });
+        // qrLocal = new QRCode(qrLocalDiv, {
+        //     text: localIP,
+        //     width: 128,
+        //     height: 128,
+        //     colorDark: "#000000",
+        //     colorLight: "#ffffff"
+        // });
+        qrLocal = createImage(qrAPI + "?data=http://" + localIP + requestURL + "&size=" + qrSize);
+    } else {
+        image(qrLocal, 0, 0);
     }
 
     if (publicIP != "" && qrPublic == null) {
-        qrPublic = new QRCode(qrPublicDiv, {
-            text: publicIP,
-            width: 128,
-            height: 128,
-            colorDark: "#000000",
-            colorLight: "#ffffff"
-        });
+        // qrPublic = new QRCode(qrPublicDiv, {
+        //     text: publicIP,
+        //     width: 128,
+        //     height: 128,
+        //     colorDark: "#000000",
+        //     colorLight: "#ffffff"
+        // });
+        qrPublic = createImage(qrAPI + "?data=http://" + publicIP + requestURL + "&size=" + qrSize);
+    } else {
+        image(qrPublic, 300, 0);
     }
 
     fill(255, 0, 0);
